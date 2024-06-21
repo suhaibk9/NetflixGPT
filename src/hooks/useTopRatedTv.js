95239b41be90f45317e919dbb8b9ce1f91bb4bd7
Suhaib Khan <suhaib0900@gmail.com>
2024-06-21 08:01:29 +0100

    Final Without Resp

--- src/hooks/useTopRatedTv.js
+++ src/hooks/useTopRatedTv.js
@@ -1,19 +1,4 @@
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization:
-//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzhlOTZkZTZjYTAwNjAwOTI5ODA2NzFiNmUyZmJjYiIsInN1YiI6IjY2NzJiOGMwYmI4ZjVmNGQzNjkwZGM0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aA8hS7Ylo8GkhBIhu58L4CxQzAxO7VD-a_6DM7RuVs8',
-//   },
-// };
 
-// fetch(
-//   'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1',
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTopRatedTv } from '../utils/tvSlice';
